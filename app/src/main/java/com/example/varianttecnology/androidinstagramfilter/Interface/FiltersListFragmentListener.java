41a829fc2fd24b1e3579cda48772d33b914305b7
Koushik Saha <Koushik.saha666@gmail.com>
2019-01-26 23:42:59 +0600

    Add All Filters with design and code and Brightness & Constraint & Saturation

---
+++ app/src/main/java/com/example/varianttecnology/androidinstagramfilter/Interface/FiltersListFragmentListener.java
@@ -0,0 +1,7 @@
+package com.example.varianttecnology.androidinstagramfilter.Interface;
+
+import com.zomato.photofilters.imageprocessors.Filter;
+
+public interface FiltersListFragmentListener {
+    void onFilterSelected(Filter filter);
+}
